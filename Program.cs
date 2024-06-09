f1f13f18c8db3705a3b9cb6cd970f241d89d7317
Martin <martinisaiasbrandalisijimenez@gmail.com>
2024-06-09 04:18:39 -0300

    tp6

--- Program.cs
+++ Program.cs
@@ -1,1 +1,41 @@
-﻿probando
+﻿while (true) {
+    Console.WriteLine("Elija la operación:");
+    Console.WriteLine("1. Sumar");
+    Console.WriteLine("2. Restar");
+    Console.WriteLine("3. Multiplicar");
+    Console.WriteLine("4. Dividir");
+    Console.WriteLine("0. Salir");
+    Console.Write("Op: ");
+
+    if (int.TryParse(Console.ReadLine(), out int sel) && sel >= 0 && sel <= 4) {
+        if (sel == 0) break;
+
+        float A, B;
+
+        while (true) {
+            Console.Write("Ingrese número A: ");
+
+            if (!float.TryParse(Console.ReadLine(), System.Globalization.NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture, out A)) Console.WriteLine("Entrada incorrecta");
+            else break;
+        }
+
+        while (true) {
+            Console.Write("Ingrese número B: ");
+
+            if (!float.TryParse(Console.ReadLine(), System.Globalization.NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture, out B)) Console.WriteLine("Entrada incorrecta");
+            else break;
+        }
+
+        Console.Write("Resultado: ");
+
+        switch (sel) {
+            case 2: Console.WriteLine(A-B); break;
+            case 3: Console.WriteLine(A*B); break;
+            case 4: Console.WriteLine(A/B); break;
+            default: Console.WriteLine(A+B); break;
+        }
+
+        Console.WriteLine();
+    } else Console.WriteLine("Entrada incorrecta");
+}
+
